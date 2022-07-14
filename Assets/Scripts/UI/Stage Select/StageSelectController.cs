efb59979168750cab4b5cc0b38800db9f0f8ed37
Victor Gabler <vfgabler@gmail.com>
2022-07-14 17:20:29 -0300

    Stage Select com dados mockup, novos icones

---
+++ Assets/Scripts/UI/Stage Select/StageSelectController.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Yaw.Data;
+
+namespace Yaw.StageSelect
+{
+    /// <summary>
+    /// Atualiza a tela de stage select, e chama o serviço de iniciar jogo ao selecionar uma fase
+    /// </summary>
+    public class StageSelectController : MonoBehaviour
+    {
+        public StageSelectEntry prefab;
+        public Transform entriesContainer;
+
+        IDataProvider<StageData> dataProvider;
+
+        List<StageSelectEntry> entries = new List<StageSelectEntry>();
+
+        //TODO melhor ser reativo
+        private void Start()
+        {
+            dataProvider = ServiceLocator.Get<IDataProvider<StageData>>();
+            UpdateData();
+        }
+
+        /// <summary>
+        /// Cria novas entradas de acordo com a necessidade, e atualiza os dados
+        /// </summary>
+        void UpdateData()
+        {
+            var stages = dataProvider.GetAll();
+            for (int i = 0; i < stages.Count; i++)
+            {
+                var entry = GetEntry(i);
+                entry.SetUp(stages[i], i);
+            }
+
+            //Desabilita as entradas extras
+            for (int i = stages.Count; i < entries.Count; i++)
+            {
+                entries[i].gameObject.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// Retorna uma entry para o indice. Pooling básico
+        /// </summary>
+        StageSelectEntry GetEntry(int index)
+        {
+            StageSelectEntry entry;
+            //Se já existe uma entry, usa-a
+            if (entries.Count > index)
+            {
+                entry = entries[index];
+            }
+            //Se não, cria nova
+            else
+            {
+                entry = Instantiate(prefab, entriesContainer);
+                entry.OnPicked += OnPicked;
+                entries.Add(entry);
+            }
+
+            //Ativa
+            entry.gameObject.SetActive(true);
+            return entry;
+        }
+
+
+        /// <summary>
+        /// Evento chamado pelas entries, quando selecionadas
+        /// </summary>
+        private void OnPicked(StageData obj)
+        {
+            //TODO levar para a cena
+            Debug.Log("Entrando na fase!");
+        }
+
+        private void OnDestroy()
+        {
+            //Remove o link do evento
+            foreach (var e in entries)
+            {
+                e.OnPicked -= OnPicked;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Stage Select/StageSelectEntry.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Yaw.Data;
+
+namespace Yaw.StageSelect
+{
+    /// <summary>
+    /// Entrada do seletor de fase. Atualiza os dados do UI
+    /// </summary>
+    public class StageSelectEntry : MonoBehaviour
+    {
+        public event Action<StageData> OnPicked;
+
+        public Text label;
+        public Button button;
+        public GameObject lockedIndicator;
+        public GameObject completedIndicator;
+
+        StageData data;
+
+        public void SetUp(StageData data, int index)
+        {
+            this.data = data;
+            completedIndicator.SetActive(data.Completed);
+            lockedIndicator.SetActive(data.Locked);
+            label.text = $"{index + 1}";
+            button.interactable = !data.Locked;
+        }
+
+        public void Pick()
+        {
+            OnPicked?.Invoke(data);
+        }
+    }
+}
